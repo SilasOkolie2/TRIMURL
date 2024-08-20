3514606ebbb0d4507627d6f748a2d886c178f5e6
Silas Okolie <159030284+SilasOkolie2@users.noreply.github.com>
2024-08-20 22:22:25 +0100

    made little changes on the footer logo path

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
-import facebook from "../images/icon-facebook.svg"
-import twitter from "../images/icon-twitter.svg"
-import pinterest from "../images/icon-pinterest.svg"
-import instagram from "../images/icon-instagram.svg"
-import logo from "../images/logo.png"
+import facebook from "../images/icon-facebook.svg";
+import twitter from "../images/icon-twitter.svg";
+import pinterest from "../images/icon-pinterest.svg";
+import instagram from "../images/icon-instagram.svg";
+import logo from "../images/logo.png";
+
 
 
 export default function Footer() {
